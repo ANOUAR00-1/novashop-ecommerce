5fb9714f1413fc7091a42f6266973bbed2a9ee0f
Anouar <b.anouar.officiel@gmail.com>
2025-11-19 12:35:18 +0000

    feat: create main application structure
    - Setup React root with main.tsx
    - Create App component with routing
    - Configure React Router v6
    - Add error boundaries

--- App.tsx
+++ App.tsx
@@ -4,6 +4,8 @@ import { Provider } from 'react-redux';
 import { store } from './store';
 import { AuthProvider } from './contexts/AuthContext';
 import { ThemeProvider } from './contexts/ThemeContext';
+import { ToastProvider } from './contexts/ToastContext';
+import { LanguageProvider } from './contexts/LanguageContext';
 import { Toaster } from 'sonner';
 import { validateStartup, logValidationResults } from './utils/startupValidator';
 
@@ -14,7 +16,10 @@ import AdminLayout from './layouts/AdminLayout';
 // Pages
 import HomePage from './pages/HomePage';
 import TestPage from './pages/TestPage';
+import AuthDebugPage from './pages/AuthDebugPage';
 import ProductsPage from './pages/ProductsPage';
+import AboutPage from './pages/AboutPage';
+import BlogPage from './pages/BlogPage';
 import ProductDetailPage from './pages/ProductDetailPage';
 import CartPage from './pages/CartPage';
 import CheckoutPage from './pages/CheckoutPage';
@@ -25,8 +30,25 @@ import ForgotPasswordPage from './pages/auth/ForgotPasswordPage';
 import ProfilePage from './pages/user/ProfilePage';
 import OrdersPage from './pages/user/OrdersPage';
 import WishlistPage from './pages/user/WishlistPage';
+import SettingsPage from './pages/user/SettingsPage';
 import NotFoundPage from './pages/NotFoundPage';
 import DiagnosticPage from './pages/DiagnosticPage';
+
+// New Feature Pages
+import ComparisonPage from './pages/ComparisonPage';
+import AboutUsPage from './pages/AboutUsPage';
+import OrderTrackingPage from './pages/OrderTrackingPage';
+import DealsPage from './pages/DealsPage';
+import CategoriesPage from './pages/CategoriesPage';
+
+// Footer Pages
+import PrivacyPolicyPage from './pages/PrivacyPolicyPage';
+import TermsOfServicePage from './pages/TermsOfServicePage';
+import CookiePolicyPage from './pages/CookiePolicyPage';
+import ContactUsPage from './pages/ContactUsPage';
+import ShippingInfoPage from './pages/ShippingInfoPage';
+import ReturnsPage from './pages/ReturnsPage';
+import FAQPage from './pages/FAQPage';
 
 // Admin Pages
 import AdminDashboard from './pages/admin/AdminDashboard';
@@ -35,10 +57,12 @@ import AdminOrders from './pages/admin/AdminOrders';
 import AdminUsers from './pages/admin/AdminUsers';
 import AdminCoupons from './pages/admin/AdminCoupons';
 import AdminAnalytics from './pages/admin/AdminAnalytics';
+import AdminReviews from './pages/admin/AdminReviews';
 
 // Components
 import ProtectedRoute from './components/ProtectedRoute';
 import ErrorBoundary from './components/ErrorBoundary';
+import ScrollToTop from './components/ScrollToTop';
 
 function AppContent() {
   useEffect(() => {
@@ -76,21 +100,42 @@ function AppContent() {
 
   return (
     <>
+      <ScrollToTop />
       <Toaster position="top-right" richColors />
       <Routes>
         {/* Test Route - Temporary */}
         <Route path="/test" element={<TestPage />} />
+        <Route path="/auth-debug" element={<AuthDebugPage />} />
         
         {/* Public Routes */}
         <Route path="/" element={<MainLayout />}>
           <Route index element={<HomePage />} />
           <Route path="products" element={<ProductsPage />} />
           <Route path="products/:id" element={<ProductDetailPage />} />
+          <Route path="about" element={<AboutPage />} />
+          <Route path="blog" element={<BlogPage />} />
+          <Route path="contact" element={<ContactUsPage />} />
           <Route path="cart" element={<CartPage />} />
           <Route path="login" element={<LoginPage />} />
           <Route path="register" element={<RegisterPage />} />
           <Route path="forgot-password" element={<ForgotPasswordPage />} />
           <Route path="diagnostic" element={<DiagnosticPage />} />
+          
+          {/* Footer Pages */}
+          <Route path="privacy-policy" element={<PrivacyPolicyPage />} />
+          <Route path="terms-of-service" element={<TermsOfServicePage />} />
+          <Route path="cookie-policy" element={<CookiePolicyPage />} />
+          <Route path="contact" element={<ContactUsPage />} />
+          <Route path="shipping-info" element={<ShippingInfoPage />} />
+          <Route path="returns" element={<ReturnsPage />} />
+          <Route path="faq" element={<FAQPage />} />
+          
+          {/* Feature Pages */}
+          <Route path="comparison" element={<ComparisonPage />} />
+          <Route path="about" element={<AboutUsPage />} />
+          <Route path="track-order" element={<OrderTrackingPage />} />
+          <Route path="deals" element={<DealsPage />} />
+          <Route path="categories" element={<CategoriesPage />} />
           
           {/* Protected User Routes */}
           <Route path="checkout" element={
@@ -118,6 +163,11 @@ function AppContent() {
               <WishlistPage />
             </ProtectedRoute>
           } />
+          <Route path="settings" element={
+            <ProtectedRoute>
+              <SettingsPage />
+            </ProtectedRoute>
+          } />
         </Route>
 
         {/* Admin Routes */}
@@ -131,6 +181,7 @@ function AppContent() {
           <Route path="orders" element={<AdminOrders />} />
           <Route path="users" element={<AdminUsers />} />
           <Route path="coupons" element={<AdminCoupons />} />
+          <Route path="reviews" element={<AdminReviews />} />
           <Route path="analytics" element={<AdminAnalytics />} />
         </Route>
 
@@ -147,13 +198,17 @@ export default function App() {
   return (
     <ErrorBoundary>
       <Provider store={store}>
-        <ThemeProvider>
-          <AuthProvider>
-            <Router>
-              <AppContent />
-            </Router>
-          </AuthProvider>
-        </ThemeProvider>
+        <LanguageProvider>
+          <ThemeProvider>
+            <ToastProvider>
+              <AuthProvider>
+                <Router>
+                  <AppContent />
+                </Router>
+              </AuthProvider>
+            </ToastProvider>
+          </ThemeProvider>
+        </LanguageProvider>
       </Provider>
     </ErrorBoundary>
   );
